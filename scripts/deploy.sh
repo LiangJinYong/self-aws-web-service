009c67f1cb9e542bb1671fba4c33343ea544fa29
ljy <393941932@qq.com>
2020-09-20 01:24:48 +0900

    Fix deploy

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -27,4 +27,7 @@ JAR_NAME=$(ls $REPOSITORY/ | grep *.jar | tail -n 1)
 
 echo "> JAR Name: $JAR_NAME"
 
-nohup java -jar $REPOSITORY/$JAR_NAME 2>&1 &
+nohup java -jar \
+    -Dspring.config.location=classpath:/application.properties,classpath:/application-real.properties,/home/ec2-user/app/application-oauth.properties./home/ec2-user/app/application-real-db.properties \
+    -Dspring.profiles.active=real \
+    $JAR_NAME > $REPOSITORY/nohup.out 2>&1 &
